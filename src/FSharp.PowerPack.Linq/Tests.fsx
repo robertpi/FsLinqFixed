7fbd17f2838c28a5a19de48dfef1c1dafbd64053
David Dickson <davidkdicson@gmail.com>
2011-06-22 19:42:15 +1000

    Added test case to ensure that System.String.op_Equality is used for comparison.

--- src/FSharp.PowerPack.Linq/Tests.fsx
+++ src/FSharp.PowerPack.Linq/Tests.fsx
@@ -4,7 +4,12 @@
 open System.Linq
 open Microsoft.FSharp.Linq
 open Microsoft.FSharp.Linq.Query
- 
+open Microsoft.FSharp.Quotations
+open Microsoft.FSharp.Linq.QuotationEvaluation
+open System.Linq.Expressions
+open System
+
+System.Diagnostics.Debugger.Launch()
 
 let makeQueryable (items: seq<_>) = 
     let temp = new ResizeArray<_>(items)
@@ -14,4 +19,16 @@ let items1 = makeQueryable [ 1 .. 100 ]
 let test1 = query <@  items1 |> Seq.skip 90  @>
 
 let items2 = makeQueryable [ for x in 1 .. 100 do yield x / 10, x]
-let test1 = query <@  items1 |> Seq.skip 90  @>
+//let test1 = query <@  items1 |> Seq.skip 90  @>
+
+let items3 = makeQueryable ["sam"; "tom"; "john"]
+
+let test3 = 
+  let ToLinq (exp : Expr<'a -> 'b>) =
+    let linq = exp.ToLinqExpression()
+    let call = linq :?> MethodCallExpression
+    let lambda = call.Arguments.[0] :?> LambdaExpression
+    Expression.Lambda<Func<'a, 'b>>(lambda.Body, lambda.Parameters)
+  let convert = ToLinq (<@ fun u -> u = "sam" @>)
+  convert.Body
+
